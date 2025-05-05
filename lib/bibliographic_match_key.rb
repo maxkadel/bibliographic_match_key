35ff1a1d94f8d3f512952daa5c7704a59cde4e6b
Max Kadel <mkadel@princeton.edu>
2025-05-05 15:56:17 -0400

    Scaffold commit

---
+++ lib/bibliographic_match_key.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require_relative "bibliographic_match_key/version"
+require_relative "bibliographic_match_key/bibliographic_match_key"
+
+module BibliographicMatchKey
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/bibliographic_match_key/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module BibliographicMatchKey
+  VERSION = "0.1.0"
+end
